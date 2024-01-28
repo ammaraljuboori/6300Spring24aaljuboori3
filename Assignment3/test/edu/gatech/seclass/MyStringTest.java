44d4a6ef30b5b9029fe5bb040f9ebb806bb7771a
Ammar Aljuboori <aaljuboori3@gatech.edu>
2024-01-27 22:14:53 -0500

    Added Assignment3

---
+++ Assignment3/test/edu/gatech/seclass/MyStringTest.java
@@ -0,0 +1,16 @@
+package edu.gatech.seclass;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.Timeout.ThreadMode;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+
+
+
